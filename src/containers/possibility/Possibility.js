291dadc6fd7cdea8b3ab321c357846975a0fb659
omarnaboush122 <omarnaboush89@gmail.com>
2022-12-06 12:37:29 -0800

    create possibility section

--- src/containers/possibility/Possibility.js
+++ src/containers/possibility/Possibility.js
@@ -1,9 +1,18 @@
 import "./possibility.css";
+import possibilityImage from "../../assets/Feature Image.png";
 
 const Possibility = () => {
   return (
-    <div>
-      possibility
+    <div className="gpt3-possibility section-padding" id="possibility">
+      <div className="gpt3-possibility-image">
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+      <div className="gpt3-possibility-content">
+        <h4>Request Early Access to Get Started</h4>
+        <h1>The possibilities are beyond your imagination</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
     </div>
   );
 }
