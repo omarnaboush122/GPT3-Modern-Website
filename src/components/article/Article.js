1ae159e1ea6a3cb29c7b12bdb00a96ba3117fc70
omarnaboush122 <omarnaboush89@gmail.com>
2022-12-06 15:11:09 -0800

    create the articles

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -1,10 +1,17 @@
 import "./article.css";
 
-const Article = ({imgUrl}) => {
+const Article = ({imgUrl,date,title}) => {
   return (
     <div className="gpt3-blog-container-article">
       <div className="gpt3-blog-container-article-image">
-        <img src={imgUrl} alt="blog image" />
+        <img src={imgUrl} alt="blog" />
+      </div>
+      <div className="gpt3-blog-container-article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
       </div>
     </div>
   );
