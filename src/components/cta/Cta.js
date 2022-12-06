fd275a683506d65844dd70aea6d58849090b045b
omarnaboush122 <omarnaboush89@gmail.com>
2022-12-06 12:54:04 -0800

    create cta section

--- src/components/cta/Cta.js
+++ src/components/cta/Cta.js
@@ -2,8 +2,14 @@ import "./cta.css";
 
 const Cta = () => {
   return (
-    <div>
-      cta
+    <div className="gpt3-cta">
+      <div className="gpt3-cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div>
+        <butyon className="gpt3-cta-button" type="button">Get Started</butyon>
+      </div>
     </div>
   );
 }
