9244464f4dd2ab505357eac322b75b0c27b0a7e8
omarnaboush122 <omarnaboush89@gmail.com>
2022-12-06 10:19:52 -0800

    create whatgpt3 section

--- src/containers/whatGPT3/WhatGPT3.js
+++ src/containers/whatGPT3/WhatGPT3.js
@@ -1,9 +1,21 @@
 import "./whatGPT3.css";
+import Feature from "../../components/feature/Feature";
 
 const WhatGPT3 = () => {
   return (
-    <div>
-      whatGPT3
+    <div className="gpt3-whatgpt3 section-margin" id="wgpt3">
+      <div className="gpt3-whatpgt3-feature">
+        <Feature/>
+      </div>
+      <div className="gpt3-whatgpt3-heading">
+        <h1 className="gradient-text">The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3-whatgpt3-container">
+        <Feature/>
+        <Feature/>
+        <Feature/>
+      </div>
     </div>
   );
 }
