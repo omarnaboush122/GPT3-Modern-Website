16f90eeadae5b156eb0a736e84a86f69b5d50cbf
omarnaboush122 <omarnaboush89@gmail.com>
2022-12-06 11:19:44 -0800

    create features and style them

--- src/components/feature/Feature.js
+++ src/components/feature/Feature.js
@@ -1,9 +1,15 @@
 import "./feature.css"
 
-const Feature = () => {
+const Feature = ({title,text}) => {
   return (
-    <div>
-      feature
+    <div className="gpt3-features-container-feature">
+      <div className="gpt3-features-container-feature-title">
+        <div></div>
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3-features-container-feature-text">
+        <p>{text}</p>
+      </div>
     </div>
   );
 }
