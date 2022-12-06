d7ec59b1c654f8ac7f55ff4cc10cde9a919f5067
omarnaboush122 <omarnaboush89@gmail.com>
2022-12-06 09:58:18 -0800

    imports images and create the brand

--- src/components/brand/Brand.js
+++ src/components/brand/Brand.js
@@ -1,9 +1,15 @@
 import "./brand.css";
-
+import { google,slack,atlassian,dropbox,shopify } from "./imports";
 const Brand = () => {
   return (
-    <div>
-      brand
+    <div className="gpt3-brand section-padding">
+      <div>
+        <img src={google} alt="google" />
+        <img src={slack} alt="slack" />
+        <img src={atlassian} alt="atlassian" />
+        <img src={dropbox} alt="dropbox" />
+        <img src={shopify} alt="shopify" />
+      </div>
     </div>
   );
 }
