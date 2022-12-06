61e3fef6012acd3deba076b53dcdda81ccb51fe4
omarnaboush122 <omarnaboush89@gmail.com>
2022-12-05 23:02:36 -0800

    create the header

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -1,11 +1,34 @@
 import "./header.css";
+import people from "../../assets/Group 81.png";
+import ai from "../../assets/Header Illustration.png";
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="gpt3-header section-padding" id="home">
+      <div className="gpt3-header-content">
+        <h1 className="gradient-text">
+          Let’s Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <div className="gpt3-header-content-input">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3-header-content-people">
+          <img src={people} alt="people" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+      <div className="gpt3-header-img">
+          <img src={ai} alt="ai" />
+        </div>
     </div>
   );
-}
+};
 
 export default Header;
