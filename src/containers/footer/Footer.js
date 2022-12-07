e3de97cf24be170d6df189c135e4cf1e0119a581
omarnaboush122 <omarnaboush89@gmail.com>
2022-12-07 10:55:21 -0800

    create footer

--- src/containers/footer/Footer.js
+++ src/containers/footer/Footer.js
@@ -1,9 +1,43 @@
 import "./footer.css";
+import logo from "../../assets/GPT-3.svg";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className="gpt3-footer section-padding">
+      <div className="gpt3-footer-heading">
+        <h1>Do you want to step in to the future before others</h1>
+      </div>
+      <div className="gpt3-footer-btn">
+        <p>Request Early Access</p>
+      </div>
+      <div className="gpt3-footer-links">
+        <div className="gpt3-footer-links-logo">
+          <img src={logo} alt="logo" />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+        <div className="gpt3-footer-links-div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3-footer-links-div">
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3-footer-links-div">
+          <h4>Get in touch</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcbs</p>
+          <p>085-132567</p>
+          <p>info@payme.net</p>
+        </div>
+      </div>
+      <div className="gpt3-footer-copyright">
+        <p>© 2021 GPT-3. All rights reserved.</p>
+      </div>
     </div>
   );
 }
